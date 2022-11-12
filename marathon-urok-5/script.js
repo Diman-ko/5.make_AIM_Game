369c8c91e6c462f08fe248e6945c232a237a21c9
Diman-ko <dr.komarov@mail.ru>
2022-11-13 00:07:53 +0300

    feat: time-list.addEventListener добавление слушателя на все кнопки

--- marathon-urok-5/script.js
+++ marathon-urok-5/script.js
@@ -1,6 +1,14 @@
 const screens = document.querySelectorAll('.screen')
-const startBtn =document.querySelector('#start')
+const startBtn = document.querySelector('#start')
+const timeList = document.querySelector('#time-list')
 startBtn.addEventListener('click', (event) => {
     event.preventDefault()
     screens[0].classList.add('up')
+})
+
+
+
+time-list.addEventListener('click', event => {
+
+
 })
