86a6554f9749e9512591a01e6432318bc439284c
Diman-ko <dr.komarov@mail.ru>
2022-11-13 00:02:37 +0300

    fix: work with button start & fix const screans

--- marathon-urok-5/script.js
+++ marathon-urok-5/script.js
@@ -1,5 +1,6 @@
-const screens = document.querySelectorAll('.screens')
+const screens = document.querySelectorAll('.screen')
 const startBtn =document.querySelector('#start')
 startBtn.addEventListener('click', (event) => {
     event.preventDefault()
+    screens[0].classList.add('up')
 })
