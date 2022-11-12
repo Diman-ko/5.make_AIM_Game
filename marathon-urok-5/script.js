34db2777d568b60dfceb88da6b0d070c83712c46
Diman-ko <dr.komarov@mail.ru>
2022-11-13 00:51:12 +0300

    feat: make timer

--- marathon-urok-5/script.js
+++ marathon-urok-5/script.js
@@ -1,6 +1,7 @@
 const startBtn = document.querySelector('#start')
 const screens = document.querySelectorAll('.screen')
 const timeList = document.querySelector('#time-list')
+const timeEl = document.querySelector('#time')
 
 let time = 0
 
@@ -15,11 +16,21 @@ timeList.addEventListener('click', event => {
     if (event.target.classList.contains('time-btn')) {
         console.log(parseInt(event.target.getAttribute('data-time')))
         time = (parseInt(event.target.getAttribute('data-time')))
+        screens[1].classList.add('up')
         startGame()
     }
 
 })
 
 function startGame () {
-    screens[1].classList.add('up')
+    setInterval(decreaseTime, 1000)
+    timeEl.innerHTML = `00:${time}`
+}
+
+function decreaseTime() {
+    let current = --time
+    if (current < 10) {
+        current = `0${current}`
+    }
+    timeEl.innerHTML = `00:${current}`
 }
